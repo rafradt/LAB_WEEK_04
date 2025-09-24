72e4477593642f8b64d4cb7acc92bfdae62d2fa5
rafradt <rafi.raditya165@gmail.com>
2025-09-24 11:03:40 +0700

    Commit No. 3 - Tabbed Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,7 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
 
 }
